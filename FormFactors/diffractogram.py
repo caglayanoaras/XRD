d6828676c7af0fe4997c07bcd00e0561bf07bc15
Caglayan Aras <caglayan_aras@hotmail.com>
2020-04-27 01:44:29 +0200

    Add superlattice estimator

---
+++ FormFactors/diffractogram.py
@@ -0,0 +1,107 @@
+import os, json, ast, random
+import pandas                 as     pd
+import numpy                  as     np
+import matplotlib.pyplot      as     plt
+
+from   XRD_ict                import XRDCalculator
+from   pymatgen.core.spectrum import Spectrum
+from   pymatgen               import Lattice, Structure, Molecule
+
+plt.rc('font', family='serif')
+plt.style.use(['seaborn-paper', 'presentation'])
+
+def create_perfect_TiAlN_structure(number_of_atoms, a, shuffle = True):
+    ''' Attributes:
+        number_of_lattice > maximum Total number of atoms in structure
+        a                 > lattice parameter
+        Returns:
+        Pymatgen structure TiAlN. Creates possible largest square supercell '''
+
+    number_of_lattice = number_of_atoms //8
+    
+    lattice = Lattice.cubic(a = a)
+    
+    matrix_coef = int(number_of_lattice**(1/3))
+    
+    coords = [[0   , 0   , 0   ],
+              [0.5 , 0.5 , 0   ],
+              [0.5 , 0   , 0.5 ],
+              [0   , 0.5 , 0.5 ],
+              [0   , 0   , 0.5 ],
+              [0.5 , 0   , 0   ],
+              [0   , 0.5 , 0   ],
+              [0.5 , 0.5 , 0.5 ],
+              ]
+    matrix = [[matrix_coef,0,0],
+              [0,matrix_coef,0],
+              [0,0,matrix_coef]]
+    
+    struct = Structure(lattice, ["Ti", "Ti", "Al", "Al","N", "N", "N", "N"], coords)
+    struct.make_supercell(matrix)
+    
+    if shuffle == True:
+        
+        site_size = len(struct)
+        element_number = int(site_size/4)
+        Al_Ti_array = ['Al'] * element_number + ['Ti'] * element_number
+        random.shuffle(Al_Ti_array)
+        for e,i in enumerate(Al_Ti_array):
+          struct.replace(e,i)
+                
+    return struct
+
+def plot_profiles_amplitudes(*args, structure = None,labellist = None):
+    ''' Attributes:
+        args      > list of different measurements in pandas form
+        structure > pymatgen structure
+        Returns:
+        plot of calculated versus measurement patterns
+    '''
+    calc = XRDCalculator(wavelength="CuKa1", typ = 'dft')
+    calc2= XRDCalculator(wavelength="CuKa1", typ = 'ict')
+    XRD = calc.get_pattern(structure,two_theta_range=(15, 105),scaled = True)
+    XRD2 = calc2.get_pattern(structure,two_theta_range=(15, 105),scaled = True)
+    
+##    for i in range(0,len(XRD.hkls)):
+##        print(XRD.hkls[i])
+##        print(XRD.y[i])    
+    fig, ax = plt.subplots()
+    ax.set_xlim([9, 105])
+    ax.set_ylim([0, 130])
+    
+    ax.scatter(XRD.x, np.multiply(XRD.y, 1.0), marker='X',color = 'b',label = 'Ab initio form factors')
+    ax.scatter(XRD2.x, np.multiply(XRD2.y, 1.0), marker='X',color = 'r',label = 'ict form factors')
+    for i,j in zip(XRD.x,np.multiply(XRD.y, 1.0)):
+        ax.plot([i,i],[0,j],linestyle = '--', color = 'b' ,alpha = 0.5)
+    for i,j in zip(XRD2.x,np.multiply(XRD2.y, 1.0)):
+        ax.plot([i,i],[0,j],linestyle = '--', color = 'b' ,alpha = 0.5)        
+    for c,i in enumerate(args):
+        if c == 0:
+            color = 'k'
+        if c == 1:
+            color = 'r'
+        if c == 2:
+            color = 'b'
+        x = i['peak_two_thetas']
+        y = i['amplitude']
+        spec = Spectrum(x,y)
+        spec.normalize(mode = 'max', value = 100)
+        ax.scatter(spec.x, spec.y, marker='X',color = color,label = labellist[c])
+        for i,j in zip(spec.x,spec.y):
+            ax.plot([i,i],[0,j],linestyle = '--', color = color ,alpha = 0.5)
+
+    if labellist != None:
+        plt.legend()
+        plt.grid()
+        
+    plt.xlabel('2\u03B8')
+    plt.ylabel('Intensity (a.u.)')
+    plt.show()
+    
+if __name__ == '__main__':
+        
+    D5000 = pd.read_excel('D5000_560.xlsx',sheet_name='DeconvSample')
+    #GADDS = pd.read_excel('GADDS_asdep.xlsx',sheet_name='DeconvSample')
+    TiAlN_perfect      = create_perfect_TiAlN_structure(700, 4.16)
+##    plot_profiles_amplitudes(GADDS,D5000, structure = TiAlN_perfect,labellist = ['GADDS','D5000'])
+    plot_profiles_amplitudes(D5000, structure = TiAlN_perfect,labellist = ['D5000'])
---
+++ FormFactors/superlattice_estimator.py
@@ -0,0 +1,100 @@
+import os, json, ast, random
+import pandas                 as     pd
+import numpy                  as     np
+import matplotlib.pyplot      as     plt
+import pymatgen               as     mg
+
+from   XRD_ict                import XRDCalculator
+from   pymatgen.core.spectrum import Spectrum
+from   pymatgen               import Lattice, Structure
+from   diffractogram          import create_perfect_TiAlN_structure, plot_profiles_amplitudes
+from   XRD_ict                import XRDCalculator                                    
+                                     
+plt.rc('font', family='serif')
+plt.style.use(['seaborn-paper', 'presentation'])
+
+def error_func(structure, measurement):
+    
+    calc = XRDCalculator(wavelength="CuKa1", typ = 'ict')
+    XRD = calc.get_pattern(structure,two_theta_range=(30, 105),scaled = True)
+    theta_calculated = (np.around(XRD.x, 1))
+    two_theta_list = [37.4, 43.5, 63.2, 75.8, 79.8, 95.6 ]
+    indexes = []
+    for two_theta in two_theta_list:
+        indexes.append(np.asscalar(np.argwhere(theta_calculated == two_theta)[0]))
+
+    intensities = XRD.y[indexes]
+    return np.square(intensities - measurement).sum()
+
+if __name__ == '__main__':
+    TiAlN_perfect      = create_perfect_TiAlN_structure(300, 4.16) #216 atom random structure
+    
+    D5000 = pd.read_excel('D5000_560.xlsx',sheet_name='DeconvSample')
+    measurement = D5000
+    
+    x = measurement['peak_two_thetas']
+    y = measurement['amplitude']
+    spec = Spectrum(x,y)
+    spec.normalize(mode = 'max', value = 100)
+    
+    EPOCHS = 100
+    UNIT_COST = 0.008
+    RISE = 1.025
+
+    
+    print('Initial error:{}'.format(error_func(TiAlN_perfect,spec.y)))
+    print('-*-*-*-*-*-')
+
+    energies    = []
+    structure   = TiAlN_perfect.copy()
+    metal_count = len(structure)//2
+    length      = len(structure)
+    
+    for eph in range(EPOCHS):
+        energy_current = error_func(structure,spec.y)
+        energies.append(energy_current)
+        
+        predicted_structure = structure.copy()
+        # Get a random index
+        ind = random.randint(0,length )
+        el = predicted_structure[ind].species.formula
+        if 'N' in el:
+            predicted_structure[ind].species = mg.Composition('V1')
+        if 'Ti'in el:
+            dice = random.randint(0,1 )
+            if dice:
+                predicted_structure[ind].species = mg.Composition('V1')
+            else:
+                predicted_structure[ind].species = mg.Composition('Al1')
+        if 'Al'in el:
+            dice = random.randint(0,1 )
+            if dice:
+                predicted_structure[ind].species = mg.Composition('V1')
+            else:
+                predicted_structure[ind].species = mg.Composition('Ti1')
+        if 'Vl'in el:
+            if ind < metal_count:
+                dice = random.randint(0,1 )
+                if dice:
+                    predicted_structure[ind].species = mg.Composition('Ti1')
+                else:
+                    predicted_structure[ind].species = mg.Composition('Al1')
+            if ind >= metal_count:
+                predicted_structure[ind].species = mg.Composition('N1')
+                
+        energy_predicted = error_func(predicted_structure,spec.y)
+
+        delta_cost = UNIT_COST*(energy_predicted - energy_current)
+        print('Iteration: ', eph)
+        print('Energy predicted:{}'.format(energy_predicted))
+        print('Energy current:{}'.format(energy_current))
+        
+        print(np.exp(-delta_cost))
+        if np.random.rand() < np.exp(-delta_cost):
+            print('switched')
+            structure = predicted_structure
+
+        UNIT_COST *= RISE
+        print('---')
+
+        
